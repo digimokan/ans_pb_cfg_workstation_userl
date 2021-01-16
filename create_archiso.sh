00150227f66cd29420d41ae3fdb1b19a3d4ed0de
digimokan <digimokan@users.noreply.github.com>
2021-01-15 23:48:20 -0600

    create_archiso: fix broken build cmd

--- create_archiso.sh
+++ create_archiso.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# change to third-party git subtree source dir
-cd third_party/make-archiso-zfs || exit 1
+MAKE_ARCHISO_CMD="$(pwd)/third_party/make-archiso-zfs/make_archiso_zfs.sh"
 
 # create Archiso live USB image
-sudo ./make-archiso-zfs.sh \
+sudo "${MAKE_ARCHISO_CMD}" \
   --build-iso \
   --enable-zfs-kernel-module \
   --extra-packages="git,ansible" \
-  --user-files="../../scripts/install_arch_linux.sh,../../scripts/install_options.yml"
+  --user-files="scripts/install_arch_linux.sh,scripts/install_options.yml"
 
